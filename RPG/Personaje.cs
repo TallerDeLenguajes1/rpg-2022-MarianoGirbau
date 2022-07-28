9644208b037e390c045eb7090e5621d39909915f
Mariano Girbau <marianogirbau@gmail.com>
2022-07-28 19:10:51 -0300

    CSV

--- RPG/Personaje.cs
+++ RPG/Personaje.cs
@@ -12,7 +12,8 @@ namespace RPG
         Humano,
         Enano,
         Elfo,
-        ElfoOscuro
+        ElfoOscuro,
+        Demonio
     }
 
     public enum NombresEnum
@@ -43,24 +44,15 @@ namespace RPG
         private DateTime fechadeNacimiento;
         private int edad;           //entre 0 a 300
         private int salud;          //100
+
         //Stats
         private int nivel;          //1 a 10
         private int velocidad;      // 1 a 10
         private int destreza;       //1 a 5
         private int fuerza;         //1 a 10
         private int armadura;       //1 a 10
+        private int batallasganadas = 0;  //batallas ganadas
 
-       /* public Personaje()
-        {
-            Nivel = 1;
-            Salud = 100;
-        }*/
-
-         public int TirarDodecaedro()
-        {
-            Random rnd = new Random();
-            return (rnd.Next(1, 13));
-        }
 
         public TipoDeClase Clase { get => clase; set => clase = value; }
         public NombresEnum Nombre { get => nombre; set => nombre = value; }
@@ -73,6 +65,47 @@ namespace RPG
         public int Fuerza { get => fuerza; set => fuerza = value; }
         public int Nivel { get => nivel; set => nivel = value; }
         public int Armadura { get => armadura; set => armadura = value; }
+        public int BatallasGanadas { get => batallasganadas; set => batallasganadas = value; }
+
+
+         /******************Creacion aleatoria*********************/
+        public void creacionAleatoria()
+        {
+            var rnd = new Random();
+            //Eleccion de clase aleatoria
+            var Clases = Enum.GetValues(typeof(TipoDeClase));
+            TipoDeClase Clase = (TipoDeClase)Clases.GetValue(rnd.Next(Clases.Length)); //Elije aleatoriamente de un enum con las clases
+            this.Clase=Clase;
+
+            //Nivel del personaje
+            Nivel = 1;
+
+            //Eleccion de stats aleatorios
+            Salud = 100;
+            Velocidad = rnd.Next(1, 11);
+            Destreza = rnd.Next(1, 6);
+            Fuerza = rnd.Next(1, 11);
+            Armadura = rnd.Next(1, 11);
+
+            //Eleccion de datos aleatorios
+            var Nombres = Enum.GetValues(typeof(NombresEnum));
+            NombresEnum Nombre = (NombresEnum)Nombres.GetValue(rnd.Next(Nombres.Length));
+            this.Nombre = Nombre;
+            var Apodos = Enum.GetValues(typeof(ApodosEnum));
+            ApodosEnum Apodo = (ApodosEnum)Apodos.GetValue(rnd.Next(Apodos.Length));
+            this.Apodo = Apodo;
+            //Calculo de edad
+            FechadeNacimiento = new DateTime (rnd.Next(1722,2023),rnd.Next(1,13),rnd.Next(1,29));
+            DateTime fechaActual= DateTime.Now;
+            if (fechaActual.Month >= FechadeNacimiento.Month && fechaActual.Day >= FechadeNacimiento.Day)
+            {
+                Edad = fechaActual.Year - FechadeNacimiento.Year;
+            }
+            else
+            {
+                Edad = (fechaActual.Year - FechadeNacimiento.Year)-1;
+            }
+        }
 
         //Mostrar personaje
         public void mostrarPersonaje()
@@ -90,6 +123,29 @@ namespace RPG
             Console.WriteLine("Armadura: " + armadura);
         } 
         
+         public void Descansar()
+            {
+                if (Salud < 85)
+                {
+                    Salud += 15;
+                }
+                else
+                {
+                    Salud = 100;
+                }
+                if (Nivel < 10)
+                {
+                    Nivel++;
+                    
+                }
+                BatallasGanadas++;
+            }
+
+         public int TirarDodecaedro()
+        {
+            Random rnd = new Random();
+            return (rnd.Next(1, 13));
+        }
 
     }
 }
--- RPG/Program.cs
+++ RPG/Program.cs
@@ -4,15 +4,16 @@ namespace MyProgram {
     {
         public static void Main()
         {   
+            MostrarHistorialGanadores();
+
             int cantidadParticipantes=4;
             List<Personaje> Participantes = new List<Personaje>();
-
             //Carga de Personajes
             Console.WriteLine("\n##### Participantes #####");
             for (int i = 0; i < cantidadParticipantes; i++)
             {
                 Personaje personaje = new Personaje();
-                personaje = creacionAleatoria();
+                personaje.creacionAleatoria();
                 Participantes.Add(personaje);
                 personaje.mostrarPersonaje();
                 Console.WriteLine("####################");
@@ -20,65 +21,37 @@ namespace MyProgram {
 
             //Peleas
             Console.WriteLine("\n##### Batallas #####");
-            List<Personaje> Ganadores = new List<Personaje>();
-            for (int i = 0; i <= (Participantes.Count)/2; i=i+2) //Batallas de a pares
-            {   
-                Personaje Ganador = Batalla(Participantes.ElementAt(i), Participantes.ElementAt(i + 1));
-                Ganadores.Add(Ganador);
-            }
-            for (int i = 0; i <= (Ganadores.Count)/2; i=i+2)
+            do
             {
-                List<Personaje> FixtureActual = Ganadores;
-                Ganadores.Clear();
-            }
+                List<Personaje> Ganadores = new List<Personaje>();
+                for (int j = 0; j <= (Participantes.Count)/2; j=j+2) //Batallas de a pares
+                {   
+                    Personaje Ganador = Batalla(Participantes.ElementAt(j), Participantes.ElementAt(j + 1));
+                    Ganador.Descansar(); //recupera vida y gana nivel
+                    Ganadores.Add(Ganador);
+                }
+                Participantes.RemoveAll(x => !(Ganadores.Contains(x)));
+            } while (Participantes.Count>1);
+
+            Console.WriteLine("\nEl ganador del torneo es:");
+            var ganador = Participantes[0];
+            Console.WriteLine($"{ganador.Apodo} {ganador.Nombre} !!!\n");
+            PresionarTecla();
+            Console.WriteLine("###### Stats #####");
+            ganador.mostrarPersonaje();
+
+            //Guardado de ganador
+            string path = Directory.GetCurrentDirectory();
+            string nombreArchivo = path + "/Ganadores.csv"; //donde se guardan los archivos
+            string datosGanador = $"{ganador.Apodo};{ganador.Nombre};{ganador.Nivel};{ganador.BatallasGanadas}%TAB;{DateTime.Today.ToShortDateString()}";
+            HelperDeArchivos.GuardarArchivoCsv(nombreArchivo,datosGanador);
         }
 
-        //Creacion aleatoria
-        static Personaje creacionAleatoria()
-        {
-            Personaje personajeAux = new Personaje();
-            var rnd = new Random();
-            //Eleccion de clase aleatoria
-            var Clases = Enum.GetValues(typeof(TipoDeClase));
-            TipoDeClase Clase = (TipoDeClase)Clases.GetValue(rnd.Next(Clases.Length)); //Elije aleatoriamente de un enum con las clases
-            personajeAux.Clase = Clase;
-
-            //Nivel del personaje
-            personajeAux.Nivel = 1;
-
-            //Eleccion de stats aleatorios
-            personajeAux.Salud = 100;
-            personajeAux.Velocidad = rnd.Next(1, 11);
-            personajeAux.Destreza = rnd.Next(1, 6);
-            personajeAux.Fuerza = rnd.Next(1, 11);
-            personajeAux.Armadura = rnd.Next(1, 11);
-
-            //Eleccion de datos aleatorios
-            var Nombres = Enum.GetValues(typeof(NombresEnum));
-            NombresEnum Nombre = (NombresEnum)Nombres.GetValue(rnd.Next(Nombres.Length));
-            personajeAux.Nombre = Nombre;
-            var Apodos = Enum.GetValues(typeof(ApodosEnum));
-            ApodosEnum Apodo = (ApodosEnum)Apodos.GetValue(rnd.Next(Apodos.Length));
-            personajeAux.Apodo = Apodo;
-            //Calculo de edad
-            personajeAux.FechadeNacimiento = new DateTime (rnd.Next(1722,2023),rnd.Next(1,13),rnd.Next(1,29));
-            DateTime fechaActual= DateTime.Now;
-            if (fechaActual.Month >= personajeAux.FechadeNacimiento.Month && fechaActual.Day >= personajeAux.FechadeNacimiento.Day)
-            {
-                personajeAux.Edad = fechaActual.Year - personajeAux.FechadeNacimiento.Year;
-            }
-            else
-            {
-                personajeAux.Edad = (fechaActual.Year - personajeAux.FechadeNacimiento.Year)-1;
-            }
-
-            return personajeAux;
-        }
-
-        //Datos Batalla
-         static Personaje Batalla(Personaje PJ1,Personaje PJ2)
+        /******************* Batalla *********************/
+        static Personaje Batalla(Personaje PJ1,Personaje PJ2)
         {   
-            Console.WriteLine($"Pelean: {PJ1.Apodo} {PJ1.Nombre} vs {PJ2.Apodo} {PJ2.Nombre}");
+            Console.WriteLine($"PELEAN: {PJ1.Apodo} {PJ1.Nombre} vs {PJ2.Apodo} {PJ2.Nombre}!!!");
+            Console.WriteLine($"Suerte a ambos contricantes\n");
             int MDP = 50000;
             Random rnd = new Random();
             for (int i = 1; i < 3 && PJ1.Salud>=0 && PJ2.Salud>=0; i++)
@@ -94,9 +67,9 @@ namespace MyProgram {
                 float ValorDeAtaque2 = PoderDeDisparo2 * EfectividadDeDisparo2;
                 float PoderDeDefensa2 = PJ2.Armadura * PJ2.Velocidad;
 
-                if (PJ2.Salud >= 0 && PJ1.Salud>= 0) //Para que no pegue si estan muerto
+                if (PJ2.Salud >= 0 && PJ1.Salud>= 0) //Para que no pegue si esta muerto
                 {
-                    float DA1 = (((ValorDeAtaque1 * EfectividadDeDisparo1) - PoderDeDefensa2) / MDP) * 100;
+                    float DA1 = (((ValorDeAtaque1 * EfectividadDeDisparo1) - PoderDeDefensa2) / MDP) * 10f;
                     PJ2.Salud = (int)(PJ2.Salud - DA1);
                     if (DA1 > 0) 
                     {
@@ -104,7 +77,7 @@ namespace MyProgram {
                         {
                             Console.Write("Golpe crítico! ");
                         }
-                        Console.WriteLine($"{PJ2.Apodo} {PJ2.Nombre} pierde {DA1} ptos. de vida.");
+                        Console.WriteLine($"{PJ2.Apodo} {PJ2.Nombre} pierde {DA1} PV.");
                         if (PJ2.Salud <= 0)
                         {
                             Console.WriteLine($"{PJ2.Apodo} {PJ2.Nombre} no puede continuar.");
@@ -115,9 +88,12 @@ namespace MyProgram {
                         Console.WriteLine($"{PJ2.Apodo} {PJ2.Nombre} esquivó el golpe.");
                     }
                 }
-                if (PJ1.Salud >= 0 && PJ2.Salud >= 0) //Para que no pegue si estan muerto
+
+                PresionarTecla();
+
+                if (PJ1.Salud >= 0 && PJ2.Salud >= 0) //Para que no pegue si esta muerto
                 {
-                    float DA2 = (((ValorDeAtaque2 * EfectividadDeDisparo2) - PoderDeDefensa1) / MDP) * 100;
+                    float DA2 = (((ValorDeAtaque2 * EfectividadDeDisparo2) - PoderDeDefensa1) / MDP) * 10f;
                     PJ1.Salud = (int)(PJ1.Salud - DA2);
                     if (DA2 > 0)
                     {
@@ -125,7 +101,7 @@ namespace MyProgram {
                         {
                             Console.Write("Golpe crítico! ");
                         }
-                        Console.WriteLine($"{PJ1.Apodo} {PJ1.Nombre} pierde {DA2} ptos. de vida.");
+                        Console.WriteLine($"{PJ1.Apodo} {PJ1.Nombre} pierde {DA2} PV.");
                         if (PJ1.Salud <= 0)
                         {
                             Console.WriteLine($"{PJ1.Apodo} {PJ1.Nombre} no puede continuar.");
@@ -136,30 +112,80 @@ namespace MyProgram {
                         Console.WriteLine($"{PJ1.Apodo} {PJ1.Nombre} esquivó el golpe.");
                     }
                 }
+
+                PresionarTecla();
             }
+
             //Ver quien quedó con menos vida
             if (PJ1.Salud > PJ2.Salud)
             {
                 Console.WriteLine($"El ganador es {PJ1.Apodo} {PJ1.Nombre}.");
+                Console.WriteLine("####################");
+                PresionarTecla();
                 return PJ1;
             }else if (PJ1.Salud < PJ2.Salud)
             {
                 Console.WriteLine($"El ganador es {PJ2.Apodo} {PJ2.Nombre}.");
+                Console.WriteLine("####################");
+                PresionarTecla();
                 return PJ2;
             }else
             {
-                Console.WriteLine($"Es un empate, gana el que tenga mejor suerte.");
+                Console.WriteLine($"Es un empate, gana el que tenga mejor suerte:");
+                PresionarTecla();
                 int SuertePJ1 = PJ1.TirarDodecaedro();
+                Console.WriteLine($"{PJ1.Apodo} {PJ1.Nombre} saco un {SuertePJ1}.");
+                PresionarTecla();
                 int SuertePJ2 = PJ2.TirarDodecaedro();
+                Console.WriteLine($"{PJ2.Apodo} {PJ2.Nombre} saco un {SuertePJ2}.");
+                PresionarTecla();
                 if (SuertePJ1>SuertePJ2)
                 {
                     Console.WriteLine($"El ganador es {PJ1.Apodo} {PJ1.Nombre}.");
+                    Console.WriteLine("####################");
+                    PresionarTecla();
                     return PJ1;
                 }else
                 {
                     Console.WriteLine($"El ganador es {PJ2.Apodo} {PJ2.Nombre}.");
+                    Console.WriteLine("####################");
+                    PresionarTecla();
                     return PJ2;
                 }
+            }
+        }
+
+        static void PresionarTecla()
+            {
+                Console.ReadKey(true);
+            }
+
+        /******************* Historial *********************/
+        static void MostrarHistorialGanadores()
+        {
+            string opcion;
+            if (File.Exists("ganadores.csv"))
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese una opción:");
+                    Console.WriteLine("\t[1] Ver historial de ganadores");
+                    Console.WriteLine("\t[2] Jugar");
+                    Console.Write("Esperando entrada: ");
+                    opcion = Console.ReadLine();
+                } while (opcion != "1" && opcion != "2");
+            }
+            else
+            {
+                opcion = "2";
+            }
+            if (opcion == "1"){  
+                Console.WriteLine("###### HISTORIAL DE GANADORES ######");
+                Console.WriteLine("Nombre\t\t|Apodo\t\t|Nivel\t|Bat. Ganadas\t|Fecha");
+                string TextoAMostrar = HelperDeArchivos.AbrirArchivoCsv("ganadores.csv");
+                TextoAMostrar = TextoAMostrar.Replace(";", "\t|");
+                Console.WriteLine(TextoAMostrar.Replace("%TAB", "\t"));
+                PresionarTecla();
             }
         }
     }
